1230e354ab42ab2d064eeeda7010068877d0b96d
eladtur777 <eladtur777@gmail.com>
2019-12-03 23:22:35 +0200

    second

---
+++ BooksApi/Controllers/BooksController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BooksApi.Models;
+using BooksApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BookService _bookService;
+
+        public BooksController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Book>> Get() =>
+            _bookService.Get();
+
+        [HttpGet]
+        public ActionResult<Book> GetBookById(Book Book)
+        {
+            var res = _bookService.Get(Book._id);
+
+            if (res == null)
+            {
+                return NotFound();
+            }
+
+            return res;
+        }
+
+        [HttpPost]
+        public ActionResult<Book> Create(Book book)
+        {
+            _bookService.Create(book);
+
+            return CreatedAtRoute("GetBook", new { id = book._id.ToString() }, book);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Book bookIn)
+        {
+            var book = _bookService.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _bookService.Update(id, bookIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var book = _bookService.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _bookService.Remove(book._id);
+
+            return NoContent();
+        }
+    }
+}
